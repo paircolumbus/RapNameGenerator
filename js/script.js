0b9a77724586d55d9c6f70be120b4df85efabba6
Kaleb Davis <kbd6024@rit.edu>
2015-06-09 13:48:53 -0400

    rap name generator script functional

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 
-/** 
+/**
  *  RAP NAME GENERATOR
  *  The user will insert their first name and on click receive one of several
  *  possible outputs (i.e. Jill).
@@ -20,13 +20,83 @@ function Generator() {
 
 }
 
+function getRapName(name, first_names, last_names) {
+  return getRapFirstName(name, first_names) + getRapLastName(name, last_names);
+}
 
-//Add your codez here
+function getRapFirstName(name, first_names) {
+  var numOptions = first_names.length + 2;
+  var option = getRandomInt(0, numOptions);
+  if(option > numOptions) {
+    alert("Error: random number generator is wrong");
+  }
+  if(option === numOptions) {
+    return initializeName(name);
+  }
+  else if(option === numOptions-1){
+    return name;
+  }
+  else if(option === numOptions-2) {
+    return name.toUpperCase()[0];
+  }
+  else {
+    return first_names[option] + " " + name;
+  }
+}
 
+function getRapLastName(name, last_names) {
+  var numOptions = last_names.length;
+  var option = getRandomInt(0, numOptions);
+  if(option > numOptions) {
+    alert("Error: random number generator is wrong");
+  }
+  if(option === numOptions){
+    return '';
+  }
+  else {
+    return " " + last_names[option];
+  }
+}
+
+
+function initializeName(name) {
+  var initials = name.toUpperCase().split('');
+  var newName = '';
+  initials.forEach( function(letter) {
+    newName+= letter + ".";
+  });
+  return newName;
+}
+
+function verifyInput(name) {
+  return name != "";
+}
+
+function getUsersName() {
+  return $("input[id='user-input']").val();
+}
+
+/**
+ * Gets a random number between min (inclusive) and max (inclusive)
+ */
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 $(document).ready(function() {
 
     var engine = new Generator;
-    //Add your codez here
+    $("#enter").click( function() {
+      if(!verifyInput(getUsersName())) {
+        $(".response").hide();
+        $(".error").show();
+      }
+      else {
+        var rapName = getRapName(getUsersName(), engine.first_names, engine.last_names)
+        $(".error").hide();
+        $(".response").text(rapName);
+        $(".response").show();
+      }
+    });
 
 });
