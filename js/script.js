bf90ceadfbe38890f2abc7681d28eace8af2143a
Kaleb Davis <kbd6024@rit.edu>
2015-06-11 14:03:58 -0400

    Pull functionality into generator object

--- js/script.js
+++ js/script.js
@@ -18,62 +18,61 @@ function Generator() {
     this.last_names = ['the Chef', 'Digital', 'Wise', 'Knight', 'Wrecka', 'the Genius', 'the Zoo Keeper', 'the Monk', 'the Scientist', 'the Disciple', 'the Darkman', 'Pellegrino', 'the Ill Figure', 'Rocks The World', 'the Baptist',];
     this.first_names = ['Inspectah', 'Masta', 'Poppa', 'Five Foot', 'Ghostface', 'Old Dirty'];
 
-}
+    this.getRapName = function(name) {
+      return this.getRapFirstName(name) + this.getRapLastName(name);
+    }
 
-function getRapName(name, first_names, last_names) {
-  return getRapFirstName(name, first_names) + getRapLastName(name, last_names);
-}
+    this.getRapFirstName = function(name) {
+      var numOptions = this.first_names.length + 2;
+      var option = getRandomInt(0, numOptions);
+      if(option > numOptions) {
+        alert("Error: random number generator is wrong");
+      }
+      if(option === numOptions) {
+        return this.initializeName(name);
+      }
+      else if(option === numOptions-1){
+        return name;
+      }
+      else if(option === numOptions-2) {
+        return name.toUpperCase()[0];
+      }
+      else {
+        return this.first_names[option] + " " + name;
+      }
+    }
 
-function getRapFirstName(name, first_names) {
-  var numOptions = first_names.length + 2;
-  var option = getRandomInt(0, numOptions);
-  if(option > numOptions) {
-    alert("Error: random number generator is wrong");
-  }
-  if(option === numOptions) {
-    return initializeName(name);
-  }
-  else if(option === numOptions-1){
-    return name;
-  }
-  else if(option === numOptions-2) {
-    return name.toUpperCase()[0];
-  }
-  else {
-    return first_names[option] + " " + name;
-  }
-}
-
-function getRapLastName(name, last_names) {
-  var numOptions = last_names.length;
-  var option = getRandomInt(0, numOptions);
-  if(option > numOptions) {
-    alert("Error: random number generator is wrong");
-  }
-  if(option === numOptions){
-    return '';
-  }
-  else {
-    return " " + last_names[option];
-  }
-}
+    this.getRapLastName = function(name) {
+      var numOptions = this.last_names.length;
+      var option = getRandomInt(0, numOptions);
+      if(option > numOptions) {
+        alert("Error: random number generator is wrong");
+      }
+      if(option === numOptions){
+        return '';
+      }
+      else {
+        return " " + this.last_names[option];
+      }
+    }
 
 
-function initializeName(name) {
-  var initials = name.toUpperCase().split('');
-  var newName = '';
-  initials.forEach( function(letter) {
-    newName+= letter + ".";
-  });
-  return newName;
+    this.initializeName = function(name) {
+      var initials = name.toUpperCase().split('');
+      var newName = '';
+      initials.forEach( function(letter) {
+        newName+= letter + ".";
+      });
+      return newName;
+    }
+
+    this.getUsersName = function() {
+      return $("input[id='user-input']").val();
+    }
 }
 
 function verifyInput(name) {
   return name != "";
-}
-
-function getUsersName() {
-  return $("input[id='user-input']").val();
 }
 
 /**
@@ -87,12 +86,12 @@ $(document).ready(function() {
 
     var engine = new Generator;
     $("#enter").click( function() {
-      if(!verifyInput(getUsersName())) {
+      if(!verifyInput(engine.getUsersName())) {
         $(".response").hide();
         $(".error").show();
       }
       else {
-        var rapName = getRapName(getUsersName(), engine.first_names, engine.last_names)
+        var rapName = engine.getRapName(engine.getUsersName());
         $(".error").hide();
         $(".response").text(rapName);
         $(".response").show();
