99fe8c119ae0ec3ae9c1d07fdd494606714cf2ce
Bryon Wilkins <bwilkins@CMDVMOSXLT082.local>
2015-06-11 11:30:38 -0400

    completed rap generator

--- js/script.js
+++ js/script.js
@@ -20,13 +20,47 @@ function Generator() {
 
 }
 
+function generate(){
+  $(".error").hide()
+  $(".response").hide()
+  var engine = new Generator;
+  var input = $("input[type='text']").val();
+  convertToDots(input)
+  if(input == ""){
+    $(".error").show()
+  }else{
+    var usingFirst = Math.random() <= .5
+    if (Math.random() < .3){
+      input = convertToDots(input)
+    }
+    if (usingFirst){
+      first_name = engine.first_names[Math.floor(Math.random()*engine.first_names.length)]
+      $(".response").text(first_name + " " + input)
+      $(".response").show()
+    }else{
+      last_name = engine.last_names[Math.floor(Math.random()*engine.last_names.length)]
+      $(".response").text(input + " " + last_name)
+      $(".response").show()
+    }
+  }
+}
 
-//Add your codez here
+function convertToDots(name){
+  var str = name.toUpperCase();
+  var strArray = str.split("")
+  var newName = "";
+  for (var i = 0; i<strArray.length; i++){    
+    newName += strArray[i]
+    newName += "."
+  }
+  return newName
+}
+
+
 
 
 $(document).ready(function() {
-
-    var engine = new Generator;
-    //Add your codez here
-
+    $("#enter").click(generate)
 });
+
+
